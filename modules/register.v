e0d3de368587edaf692d9c4b4c4bdf72d5162386
Mohammad Hossein Yousefi <mhyousefi96@gmail.com>
2017-11-08 14:09:36 +0330

    Add gitignore

--- modules/register.v
+++ modules/register.v
@@ -1,4 +1,4 @@
-module register #(SIZE = 32) (clk, rst, regIn, regOut);
+module register #(parameter integer SIZE = 32) (clk, rst, regIn, regOut);
   input clk, rst;
   input [SIZE-1:0] regIn;
   output reg [SIZE-1:0] regOut;
