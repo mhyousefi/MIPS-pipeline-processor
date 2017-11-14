69de00d27fe508601b923002eaafae79bd8257c0
Mohammad Hossein Yousefi <mhyousefi96@gmail.com>
2017-11-14 20:20:40 +0330

    Remove unnecessary input/outputs and assign statements in MEMStage.v

--- pipeStages/MEMStage.v
+++ pipeStages/MEMStage.v
@@ -1,10 +1,7 @@
-module MEMStage (clk, rst, WB_EN, WB_EN_out, MEM_R_EN, MEM_R_EN_out, MEM_W_EN, PC, PC_out, ALU_res, ALU_res_out, ST_value, dataMem_out, dest, dest_out);
-  input clk, rst, WB_EN, MEM_R_EN, MEM_W_EN;
-  input [4:0] dest;
-  input [31:0] PC, ALU_res, ST_value;
-  output WB_EN_out, MEM_R_EN_out;
-  output [4:0] dest_out;
-  output [31:0] PC_out, ALU_res_out, dataMem_out;
+module MEMStage (clk, rst, MEM_R_EN, MEM_W_EN, ALU_res, ST_value, dataMem_out);
+  input clk, rst, MEM_R_EN, MEM_W_EN;
+  input [31:0] ALU_res, ST_value;
+  output [31:0]  dataMem_out;
 
   dataMem dataMem (
     .clk(clk),
@@ -15,10 +12,4 @@ module MEMStage (clk, rst, WB_EN, WB_EN_out, MEM_R_EN, MEM_R_EN_out, MEM_W_EN, P
     .dataIn(ST_value),
     .dataOut(dataMem_out)
   );
-
-  assign WB_EN_out = WB_EN;
-  assign MEM_R_EN_out = MEM_R_EN;
-  assign dest_out = dest;
-  assign PC_out = PC;
-  assign ALU_res_out = ALU_res;
 endmodule // MEMStage
